f9c39866b85ec3a245248aacb625411792b6a1df
kulakowskiJakob <kulakowskij08@gmail.com>
2026-07-30 14:50:54 -0400

    Enhance Encoder class for HLS streaming
    Refactor encoder to support HLS streaming with ffmpeg options.

--- encoder3.py
+++ encoder3.py
@@ -2,8 +2,6 @@ from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 
 import os
-import threading
-import time
 
 
 class Encoder:
@@ -19,48 +17,86 @@ class Encoder:
         self.bitrate = bitrate
 
         self.encoder = None
-        self.output = FfmpegOutput(
-            "-"
-        )
+        self.output = None
 
         self.running = False
-        self.thread = None
-        self.h264_file = "video.h264"
+
 
     def start(self):
 
         if self.running:
             return
 
+
+        print("[Encoder] Starting H.264/HLS encoder...")
+
+
         if not os.path.exists(self.output_directory):
             os.makedirs(self.output_directory)
 
-            picam2 = self.camera.get_camera()
 
-            self.encoder = H264Encoder(
-                bitrate=self.bitrate
-            )
+        picam2 = self.camera.get_camera()
 
-            picam2.start_encoder(
-                self.encoder,
-                self.output
-            )
 
-            self.running = True
+        self.encoder = H264Encoder(
+            bitrate=self.bitrate
+        )
+
+
+        hls_file = os.path.join(
+            self.output_directory,
+            "stream.m3u8"
+        )
+
+
+        ffmpeg_options = (
+            "-f hls "
+            "-hls_time 2 "
+            "-hls_list_size 5 "
+            "-hls_flags delete_segments "
+        )
+
+
+        self.output = FfmpegOutput(
+            ffmpeg_options + hls_file
+        )
+
+
+        picam2.start_recording(
+            self.encoder,
+            self.output
+        )
+
+
+        self.running = True
+
+
+        print("[Encoder] HLS stream created.")
+
+
 
     def stop(self):
 
         if not self.running:
             return
 
+
+        print("[Encoder] Stopping encoder...")
+
+
         picam2 = self.camera.get_camera()
-        picam2.stop_encoder()
+
+
+        picam2.stop_recording()
+
+
         self.running = False
 
-    def get_file(self):
-        return self.h264_file
+
+        print("[Encoder] Encoder stopped.")
+
+
 
     def is_running(self):
-        return self.running
 
-    
+        return self.running    
